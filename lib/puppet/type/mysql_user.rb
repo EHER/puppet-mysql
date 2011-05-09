932ae98712a8424705240b884df93d4f53e460f8
Thomas Sutton <me@thomas-sutton.id.au>
2011-05-09 16:38:56 +0800

    Tweak mysql_user code.

--- lib/puppet/type/mysql_user.rb
+++ lib/puppet/type/mysql_user.rb
@@ -4,45 +4,44 @@
 #
 
 Puppet::Type.newtype(:mysql_user) do
-	@doc = "Manage a MySQL database user."
-
-	ensurable
+  @doc = "Manage a MySQL database user."
+  ensurable
 
   # Name of the user.
-	newparam(:name) do
-		desc "The name of the MySQL database user in 'username@hostname' form."
-		
-		validate do |value|
-		  # Check that it's in the correct format
-		  unless value =~ /\w+@\w+/
-		    raise ArgumentError, "%s is not a valid MySQL database user name. Please specify 'username@hostname'." % value
-	    end
+  newparam(:name) do
+    desc "The name of the MySQL database user in 'username@hostname' form."
+
+    validate do |value|
+      # Check that it's in the correct format
+      unless value =~ /^(\w+@\w+)|%$/
+        raise ArgumentError, "%s is not valid. Please specify 'username@hostname'." % value
+      end
 
       # Check that the user portion isn't too long.
-		  user, host = value.split('@')
-		  if user.length > 16
-		    raise ArgumentError, "%s is too long. MySQL user names are limited to 16 characters." % value
-	    end
-	  end
-	end #newparam(:name)
+      user, host = value.split('@')
+      if user.length > 16
+        raise ArgumentError, "%s is too long. MySQL user names are limited to 16 characters." % value
+      end
+    end
+  end
 
 
   # Server is mainly for collecting resources on the right server.
-	newparam(:server) do
-	  desc "The MySQL server to create this user on."
+  newparam(:server) do
+    desc "The MySQL server to create this user on."
     default = :fqdn
   end #newparam(:server)
 
 
   # Password for the new user.
-	newproperty(:password_hash) do
-		desc "The password hash of the user. Use mysql_password() for creating such a hash."
-		
-		validate do |value|
-		  unless value =~ /\*[0-9A-F]{40,40}/
-		    raise ArgumentError, "%s is not a MySQL password hash."
-	    end
-	  end
-	end #newproperty(:password_hash)
+  newproperty(:password_hash) do
+    desc "The password hash of the user. Use mysql_password() for creating such a hash."
+
+    validate do |value|
+      unless value =~ /\*[0-9A-F]{40,40}/
+        raise ArgumentError, "%s is not a MySQL password hash."
+      end
+    end
+  end #newproperty(:password_hash)
 
 end
